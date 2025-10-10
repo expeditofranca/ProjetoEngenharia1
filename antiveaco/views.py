6e6cddb58fe49c0aa5c18c1a8d41db1937b02777
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2025-10-10 14:41:16 -0300

    Imports iniciais

--- antiveaco/views.py
+++ antiveaco/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Cliente, Divida, Pagamento
+from .serializers import ClienteSerializer
+
+import json
