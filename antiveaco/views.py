f13975097f65335781ea39beb70cd07833223cce
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2025-10-11 16:39:38 -0300

    criação get_dividas

--- antiveaco/views.py
+++ antiveaco/views.py
@@ -6,6 +6,15 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Cliente, Divida, Pagamento
-from .serializers import ClienteSerializer
+from .serializers import ClienteSerializer, DividaSerializer
 
 import json
+
+@api_view(['GET'])
+def get_dividas(request):
+    if request.method == 'GET':
+        dividas = Divida.objects.all()
+
+        serializer = DividaSerializer(dividas, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
