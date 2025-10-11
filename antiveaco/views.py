5df3f36e993e6ac213c5483f68ebfc746dd61184
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2025-10-11 17:46:25 -0300

    criação get_divida_by_id

--- antiveaco/views.py
+++ antiveaco/views.py
@@ -18,3 +18,15 @@ def get_dividas(request):
         serializer = DividaSerializer(dividas, many=True)
         return Response(serializer.data)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_divida_by_id(request, cod_divida):
+    try:
+        divida = Divida.objects.get(pk=cod_divida)
+    except Divida.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DividaSerializer(divida)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
