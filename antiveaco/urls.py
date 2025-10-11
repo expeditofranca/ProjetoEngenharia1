d67597e204ea819bb9caeb77b8b8f8c18f126f6c
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2025-10-11 20:19:21 -0300

    url p/ divida_manager

--- antiveaco/urls.py
+++ antiveaco/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('dividas/', views.get_dividas, name='get_all_dividas'),
     path('dividas/<int:cod_divida>/', views.get_divida_by_id, name='get_divida_by_id'),
+    path('data/', views.divida_manager, name='divida_manager'),
 ]
