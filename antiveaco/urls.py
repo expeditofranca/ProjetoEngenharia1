cf956f7fc969902a0a61dd5b0704d850d892fd93
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2025-10-13 09:57:35 -0300

    url p/ excluir divida

--- antiveaco/urls.py
+++ antiveaco/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('dividas/cadastrar/', views.divida_manager, name='divida_manager'),
     path('dividas/pesquisar/', views.get_dividas, name='pesquisar_divida'),
     path('dividas/atualizar/<int:cod_divida>/', views.divida_manager, name='atualizar_divida'),
+    path('dividas/excluir/<int:cod_divida>/', views.divida_manager, name='excluir_divida'),
 ]
