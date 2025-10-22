6a16b21bdb95e0df312a51abd01776e88bc2356f
jGean09 <josegeantlc@gmail.com>
2025-10-22 18:59:42 -0300

    Feat: Cria forms e URL para cadastro de cliente

--- antiveaco/forms.py
+++ antiveaco/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Divida
+from .models import Endereco
+from .models import Cliente
 
 class DividaForm(forms.ModelForm):
     class Meta:
         model = Divida
         fields = ['cpf_funcionario', 'cliente', 'valor', 'num_notafiscal', 'status']
+
+class EnderecoForm(forms.ModelForm):
+    class Meta:
+        model = Endereco
+        fields = ['logradouro', 'numero', 'bairro', 'cidade', 'estado', 'cep']
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'cpf', 'profissao', 'renda_familiar', 'status_cliente']
--- antiveaco/urls.py
+++ antiveaco/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('dividas/pesquisar/', views.get_dividas, name='pesquisar_divida'),
     path('dividas/atualizar/<int:cod_divida>/', views.divida_manager, name='atualizar_divida'),
     path('dividas/excluir/<int:cod_divida>/', views.divida_manager, name='excluir_divida'),
+    path('clientes/cadastrar/', views.cadastrar_cliente, name='cadastrar_cliente'),
 ]
